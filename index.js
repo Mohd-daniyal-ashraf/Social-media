29e32be538311f985e42b0e1a77643579135544e
mohd daniyal ashraf <daniyalashraf907@gmail.com>
2024-01-08 09:42:17 +0530

    dcs

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log("listen to port 8080");
+  console.log("ha bhai chal raha hai on 8080");
 });
 let posts = [
   {
